0cf96888d7092004826416806469e03edd0acc70
Андрей Кумыков <aakumykov@yandex.ru>
2016-10-14 13:12:59 +0700

    Создаётся контекстное меню

--- elements.js
+++ elements.js
@@ -1,31 +1,36 @@
 'use strict';
 
+function log(msg){
+	if (msg.join) { for (i in msg) console.log(i); }
+	else console.log(msg);
+}
+
 var app = angular.module('App',[]);
-
-app.controller('Ctrl', function($scope){
-	$scope.element;
-
-});
+app.controller('Ctrl', function($scope){});
 
 $(document).ready(function(){
+	$('BODY').click(function(){
+		removeContextMenu();
+	});
 
-	// var element1 = $('#el1');
-	// console.log(element1);
-
-	console.log(
-		angular.element(
-			$('#element1').attr('id')
-		)
-	);
-
-	// app.element.on('mousedown', function(obj){
-	// 	// obj.pressed = true;
-	// 	// console.log('pressed: '+obj.pressed);
-	// 	alert
-	// });
-
-	// element1.mouseup(function(obj){
-	// 	obj.pressed = false;
-	// 	console.log('pressed: '+obj.pressed);
-	// });
+	$('#el1').contextmenu(function(e){
+		createContextMenu(e);
+		return false;
+	});
 });
+
+function removeContextMenu(){
+	$('#context_menu').remove();
+}
+
+function createContextMenu(e, data={}){
+	removeContextMenu();
+
+	var box = $(document.createElement('div'));
+		box.attr('id','context_menu');
+		box.append('<header>Действия</header>');
+		box.css('left',e.pageX+'px');
+		box.css('top',e.pageY+'px');
+
+	$('body').append(box);
+}
